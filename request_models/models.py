74e66532b8e020c3cf9561f7b769e6170b6a0e57
abutahir <tahir@codeops.tech>
2023-08-28 22:06:24 +0530

    bug fix

--- request_models/models.py
+++ request_models/models.py
@@ -3,5 +3,5 @@ from pydantic import BaseModel
 class PostUserRequest(BaseModel):
     username: str
     email: str
-    designation: str | None
-    organization: str | None
+    designation: str | None = None
+    organization: str | None = None
