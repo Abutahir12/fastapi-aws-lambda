686d0900b8ece06d67dd9ad141b68878f2c38196
abutahir <tahir@codeops.tech>
2023-08-28 11:29:42 +0530

    Layers

--- handler.py
+++ handler.py
@@ -2,7 +2,7 @@ import json
 import os
 
 def hello(event, context):
-    print(os.environ("AWS_ACCESS_KEY_ID"))
-    print(os.environ("AWS_SECRET_ACCESS_KEY"))
+    print(os.environ["AWS_ACCESS_KEY_ID"])
+    print(os.environ["AWS_SECRET_ACCESS_KEY"])
 
     return {"statusCode": 200, "body": json.dumps("Success")}
